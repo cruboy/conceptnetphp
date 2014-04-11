c3b4002fdef356c3ca5b61c2a138696dc90f195d
linr <linr@0e54bc44-067d-d140-a5ad-a89cec78df39>
2014-04-11 03:48:09 +0000

    3D图片，图片坐标
    git-svn-id: http://localhost:8080/svn/jitongr@4 0e54bc44-067d-d140-a5ad-a89cec78df39

---
+++ picc.php
@@ -0,0 +1,61 @@
+<?php
+require_once '../init.php';
+echo "building...";
+$imgid = isset ($_GET['img']) ? intval ($_GET['img']) : '';
+echo $imgid;
+
+
+
+
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<style type="text/css"> 
+body
+{
+font-size:70%;
+font-family:verdana,helvetica,arial,sans-serif;
+}
+</style>
+
+<script type="text/javascript"> 
+function cnvs_getCoordinates(e)
+{
+x=e.clientX;
+y=e.clientY;
+document.getElementById("xycoordinates").innerHTML="Coordinates: (" + x + "," + y + ")";
+}
+ 
+function cnvs_clearCoordinates()
+{
+document.getElementById("xycoordinates").innerHTML="";
+}
+</script>
+</head>
+
+<body style="margin:0px;">
+<div id="coordiv" onmousemove="cnvs_getCoordinates(event)" onmouseout="cnvs_clearCoordinates()">
+<img src="mind.jpg" border="0" usemap="#planetmap" alt="Planets" />
+</div>
+<map name="planetmap" id="planetmap">
+  <area shape="circle" coords="180,139,14" href ="?img=1" alt="Venus" />
+  <area shape="rect" coords="200,129,161,10" href ="?img=2" alt="Mercury" />
+  <area shape="rect" coords="0,0,110,260" href ="?img=3" alt="Sun" />
+</map>
+
+
+
+<p>把鼠标悬停在下面的矩形上可以看到坐标：</p>
+
+<div id="coordiv2" style="float:left;width:199px;height:99px;border:1px solid #c3c3c3" onmousemove="cnvs_getCoordinates(event)" onmouseout="cnvs_clearCoordinates()"></div>
+<br />
+<br />
+<br />
+<div id="xycoordinates"></div>
+ 
+ 
+</body>
+</html>
